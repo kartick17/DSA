646e05896dd24e58d2f24de8104392a87cf9d301
Kartick17 <karticksadhu0@gmail.com>
2023-08-01 22:05:56 +0530

    Find Maximum and Minimun value in BST

--- BinaryTree/SearchNodeInBST.java
+++ BinaryTree/SearchNodeInBST.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import javax.sound.midi.Soundbank;
+
 public class SearchNodeInBST extends BuildTree {
 
     boolean search (Node root, int target) {
@@ -15,11 +17,33 @@ public class SearchNodeInBST extends BuildTree {
             return search(root.right, target);
     }
 
+    int minValue(Node root) {
+        if (root == null)
+            return -1;
+        Node temp = root;
+        while (temp.left != null)
+            temp = temp.left;
+
+        return temp.data;
+    }
+
+    int maxValue(Node root) {
+        if (root == null)
+            return -1;
+        Node temp = root;
+        while(temp.right != null)
+            temp = temp.right;
+
+        return temp.data;
+    }
+
     public static void main(String[] args) {
         SearchNodeInBST tree = new SearchNodeInBST();
-        int[] node = {10, 20, 5, 11, 17, 2, 4, 8, 6, 25, 15, -1};
+        int[] node = {10, 20, 5, 11, 17, 2, 4, 8, 6, 25, 15, 35, 1, -1};
         Node root = tree.buildBST(node);
         boolean ans = tree.search(root, 5);
         System.out.println(ans);
+        System.out.println("Minimum value is - " + tree.minValue(root));
+        System.out.println("Maximum value is - " + tree.maxValue(root));
     }
 }
